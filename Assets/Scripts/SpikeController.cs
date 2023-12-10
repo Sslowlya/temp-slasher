ca5ef372c2c3cebff4fe9b8e90439f0361ab8df5
Saikt <saiktstudent@gmail.com>
2023-12-10 20:26:41 +0700

    Новый коммит

--- Assets/Scripts/SpikeController.cs
+++ Assets/Scripts/SpikeController.cs
@@ -6,18 +6,18 @@ public class SpikeController : MonoBehaviour
 {
     [SerializeField] private int damage = 10;
 
-    private void OnCollisonEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            if (collision.gameObject.TryGetComponent<HealthSystem>(out healthSystem))
-            {
-                HealthSystem healthSystem = collision.gameObject.GetComponent<HealthSystem>();
-                healthSystem.ChangeHealth(-damage);
-            }
+    //private void OnCollisonEnter2D(Collision2D collision)
+   // {
+     //   if (collision.gameObject.tag == "Player")
+      //  {
+       //     if (collision.gameObject.TryGetComponent<HealthSystem>(out healthSystem))
+       //     {
+             //   HealthSystem healthSystem = collision.gameObject.GetComponent<HealthSystem>();
+            //    healthSystem.ChangeHealth(-damage);
+         //   }
            
-        }
-    }
+       // }
+   // }
     // Start is called before the first frame update
     void Start()
     {
